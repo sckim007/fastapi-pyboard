2a833b028336888b0aa5489a113ce65b785cb337
sckim007 <sckim007@gmail.com>
2023-07-18 13:00:45 +0900

    myapi 프로젝트 최초 커밋

---
+++ frontend/src/lib/store.js
@@ -0,0 +1,18 @@
+import { writable } from 'svelte/store'
+
+const persist_storage = (key, initValue) => {
+    const storedValueStr = localStorage.getItem(key)
+    const store = writable(storedValueStr != null ? JSON.parse(storedValueStr) 
+                                                  : initValue)
+    store.subscribe((val) => {
+        localStorage.setItem(key, JSON.stringify(val))
+    })
+    return store
+}
+
+export const page = persist_storage("page", 0)
+export const keyword = persist_storage("keyword", "")
+export const access_token = persist_storage("access_token", "")
+export const username = persist_storage("username", "")
+export const is_store_test = persist_storage("is_store_test", false)
+export const login_status = persist_storage("login_status", false)
